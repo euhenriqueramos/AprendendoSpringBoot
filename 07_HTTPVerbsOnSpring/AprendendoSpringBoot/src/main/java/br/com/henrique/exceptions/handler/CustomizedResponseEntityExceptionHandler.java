03d3378fa3e329ee3666294277e48f55624e60cb
euhenriqueramos <139631772+euhenriqueramos@users.noreply.github.com>
2023-08-16 12:58:36 -0300

    Finalizando Seção 8

--- 07_HTTPVerbsOnSpring/AprendendoSpringBoot/src/main/java/br/com/henrique/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 07_HTTPVerbsOnSpring/AprendendoSpringBoot/src/main/java/br/com/henrique/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.henrique.exceptions.ExceptionResponse;
-import br.com.henrique.exceptions.UnsupportedMathOperationException;
+import br.com.henrique.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -29,8 +29,8 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationException.class)
-	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
 			Exception ex, WebRequest request)
 	{
 		
@@ -38,7 +38,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				new Date(),
 				ex.getMessage(), 
 				request.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 	
 }
