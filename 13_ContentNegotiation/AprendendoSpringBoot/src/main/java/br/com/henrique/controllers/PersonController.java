a573183427b4434070c3e21b7240660ab230c49f
euhenriqueramos <139631772+euhenriqueramos@users.noreply.github.com>
2023-08-16 23:58:34 -0300

    Módulo 13 final

--- 13_ContentNegotiation/AprendendoSpringBoot/src/main/java/br/com/henrique/controllers/PersonController.java
+++ 13_ContentNegotiation/AprendendoSpringBoot/src/main/java/br/com/henrique/controllers/PersonController.java
@@ -3,7 +3,6 @@ package br.com.henrique.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.henrique.data.vo.v1.PersonVO;
 import br.com.henrique.data.vo.v2.PersonVOV2;
 import br.com.henrique.services.PersonServices;
+import br.com.henrique.utils.MediaType;
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -25,7 +25,10 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, 
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML
+			})
 	public List<PersonVO> findAll()
 	throws Exception
 	{
@@ -33,31 +36,52 @@ public class PersonController {
 	}
 	
 	@GetMapping(value = "/{id}",
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			produces = {MediaType.APPLICATION_JSON, 
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML
+					})
 	public PersonVO findById(@PathVariable(value = "id") Long id)
 			throws Exception
 	{
 		return service.findById(id);
 	}
 
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON, 
+							MediaType.APPLICATION_XML,
+							MediaType.APPLICATION_YML
+							},
+			produces = {MediaType.APPLICATION_JSON, 
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML
+					})
 	public PersonVO create(@RequestBody PersonVO person)
 			throws Exception
 	{
 		return service.create(person);
 	}
 	
-	@PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/v2", consumes = {MediaType.APPLICATION_JSON, 
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML		
+			},
+			produces = {MediaType.APPLICATION_JSON, 
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML
+					})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person)
 			throws Exception
 	{
 		return service.createV2(person);
 	}
 
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON, 
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML
+			},
+			produces = {MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML
+			})
 	public PersonVO update(@RequestBody PersonVO person)
 			throws Exception
 	{
--- 13_ContentNegotiation/AprendendoSpringBoot/src/main/java/br/com/henrique/data/vo/v1/PersonVO.java
+++ 13_ContentNegotiation/AprendendoSpringBoot/src/main/java/br/com/henrique/data/vo/v1/PersonVO.java
@@ -22,7 +22,7 @@ public class PersonVO implements Serializable {
 	private String lastName;
 	private String address;
 	
-	@JsonIgnore
+	//@JsonIgnore
 	private String gender;
 	
 	public PersonVO() 
