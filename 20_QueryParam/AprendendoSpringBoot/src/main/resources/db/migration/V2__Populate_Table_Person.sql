d8798b4b235ef4261cf9faa49e6db7f9306d1a58
euhenriqueramos <139631772+euhenriqueramos@users.noreply.github.com>
2023-09-08 22:41:36 -0300

    Final seção 19
    Finalizando os estudos da seção 19

---
+++ 20_QueryParam/AprendendoSpringBoot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name` ) VALUES 
+(1,'São Paulo','Victor','Male','Ramos'),
+(2,'São Paulo','Henrique','Male','Ramos'),
+(3,'São Paulo','Marcelo','Male','Ramos'),
+(4,'Rio de Janeiro','José','Male','Bueno'),
+(5,'Minas Gerais','João','Male','Barro'),
+(6,'Saragoza','Thiago','Male','Alfeus'),
+(7,'Jerusalém','José','Male','Arimatéia'),
+(8,'Croácia','Nikola','Male','Tesla');
---
+++ 20_QueryParam/AprendendoSpringBoot/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('leandro', 'Leandro Costa', '6f0379049b9c9a0618f5a4ddb1d19251b153764b9e53952cbac6e1546e011ea3a0ef36e8564604f3', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
